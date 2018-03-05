e0f00acf629b595931441c5a279c97df607e16bb
Hans Christian Nenseth <nenseth@gmail.com>
2018-03-05 23:44:16 +0100

    WiP

---
+++ app/src/main/java/tech/darksun/cryptov2/Constants.java
@@ -0,0 +1,8 @@
+package tech.darksun.cryptov2;
+
+/**
+ * Created by nense on 05.03.2018.
+ */
+
+public class Constants {
+}
---
+++ app/src/main/java/tech/darksun/cryptov2/MainActivity.java
@@ -0,0 +1,135 @@
+package tech.darksun.cryptov2;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+
+    private ProgressDialog pDialog;
+    private ListView lv;
+
+    private static String url = "https://api.coinmarketcap.com/v1/ticker/?limit=5";
+
+    ArrayList<HashMap<String, String>> bitcoinList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bitcoinList = new ArrayList<>();
+
+        lv = (ListView) findViewById(R.id.list);
+
+        new GetContacts().execute();
+    }
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray coins = jsonObj.getJSONArray("");
+
+                    // looping through All Coins
+                    for (int i = 0; i < coins.length(); i++) {
+                        JSONObject c = coins.getJSONObject(i);
+
+                        String id = c.getString("id");
+                        String symbol = c.getString("symbol");
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> coin = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        coin.put("id", id);
+                        coin.put("symbol", symbol);
+
+                        // adding contact to contact list
+                        bitcoinList.add(coin);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             **/
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, bitcoinList,
+                    R.layout.list_item, new String[]{"name", "symbol"},
+                    new int[]{R.id.name, R.id.symbol});
+
+            lv.setAdapter(adapter);
+        }
+    }
+}
